2d4b82e0967c981763b859e7083aa4b041cec988
madhulika95b <srisudhamadhulika.bayyavarapu@unt.edu>
2020-07-06 17:43:34 -0500

    Add Sub-year groupings to seasons

--- edtf_validate/valid_edtf.py
+++ edtf_validate/valid_edtf.py
@@ -71,7 +71,7 @@ LEVEL 1 GRAMMAR START
 """
 # Auxiliary Assignments for Level 1
 UASymbol = oneOf("? ~ %")
-seasonNumber = oneOf("21 22 23 24")
+seasonNumber = oneOf("21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41")
 season = year + "-" + seasonNumber
 dateOrSeason = season | date
 # uncertain Or Approximate Date
